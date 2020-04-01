e03daceb11fb61f0d85a784d1a68d9e75e2a5a72
Koseydon <denizhasirci@icloud.com>
2020-04-01 16:43:32 +0300

    various endpoint changes

--- src/data/UserInfos.js
+++ src/data/UserInfos.js
@@ -1,14 +1,14 @@
 //User Informations and Categories
 export default {
     authors: {
-        KesKose: 'http://localhost:3000/avatars/keskose.gif',
-        Koseydon: 'http://localhost:3000/avatars/koseydon.png',
-        Dreamcow: 'http://localhost:3000/avatars/dreamcow.png'
+        KesKose: 'https://localhost:3000/avatars/keskose.gif',
+        Koseydon: 'https://localhost:3000/avatars/koseydon.png',
+        Dreamcow: 'https://localhost:3000/avatars/dreamcow.png'
     },
     categories: {
-        Games: 'http://localhost:3000/defaultImages/games.jpg',
-        Music: 'http://localhost:3000/defaultImages/music.png',
-        Technology: 'http://localhost:3000/defaultImages/technology.png',
-        Life: 'http://localhost:3000/defaultImages/games.jpg'
+        Games: 'https://localhost:3000/defaultImages/games.jpg',
+        Music: 'https://localhost:3000/defaultImages/music.png',
+        Technology: 'https://localhost:3000/defaultImages/technology.png',
+        Life: 'https://localhost:3000/defaultImages/games.jpg'
     } 
 }
