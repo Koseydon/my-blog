fda521de78161d1abde9621969395de5391119ff
Koseydon <denizhasirci@icloud.com>
2020-04-01 20:06:12 +0300

    Endpoint Corrections

--- src/data/UserInfos.js
+++ src/data/UserInfos.js
@@ -1,14 +1,14 @@
 //User Informations and Categories
 export default {
     authors: {
-        KesKose: 'https://localhost:3000/avatars/keskose.gif',
-        Koseydon: 'https://localhost:3000/avatars/koseydon.png',
-        Dreamcow: 'https://localhost:3000/avatars/dreamcow.png'
+        KesKose: '/backend/avatars/keskose.gif',
+        Koseydon: '/backend/avatars/koseydon.png',
+        Dreamcow: '/backend/avatars/dreamcow.png'
     },
     categories: {
-        Games: 'https://localhost:3000/defaultImages/games.jpg',
-        Music: 'https://localhost:3000/defaultImages/music.png',
-        Technology: 'https://localhost:3000/defaultImages/technology.png',
-        Life: 'https://localhost:3000/defaultImages/games.jpg'
+        Games: '/backend/defaultImages/games.jpg',
+        Music: '/backend/defaultImages/music.png',
+        Technology: '/backend/defaultImages/technology.png',
+        Life: '/backend/defaultImages/games.jpg'
     } 
 }
